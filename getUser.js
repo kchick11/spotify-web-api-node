6397eb5e7a11f8185f48d4fbfd603f4aa3f0e281
kchick11 <kyle.chick@yahoo.ca>
2022-01-15 14:30:06 -0800

    Changed the limit on get playlist amount.
    3rd pust 149 mins. Small push, but a big W.   :)
    
    Co-Authored-By: mlfarn <53276499+mlfarn@users.noreply.github.com>
    Co-Authored-By: Leah Hartwell <48174088+leahhartwell@users.noreply.github.com>

--- getUser.js
+++ getUser.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const SpotifyWebApi = require('spotify-web-api-node');
-const token = "BQAMxJ603NRbUjKDfNFx7MvX-zTYAz3IreaCk1eCAFc2wK7VDY82Fo_EroC_fYgUrv_zK_9xGSpithO4_Gr_LwrkhVjMlWxJZhNj3TWS2jiPe54HQRYLZBwoyvcrwN8Y1q1zTBlYlsG7WtGBcogJPeYU0bmjHMA4gbBXVHLusnfYhvSv7aIttyrgPbD-L34ZCzEIIye24oUWO42cpNUBaoGncEi4YhrifA_FlowlhQH5oKZx2Bkk9ZZVYVRPuiD9zjXh3besWXdQyKWyhyK6u6PYHC-7XywC4PksMjo4P_jxBIy0yz5W"
+const token = "BQCFA42UtWslfrB30FflT8ZMppKKgN-jaQ5uBZEwG_y0yrham9LH5n4lPGydkEVpoc8s_EQ_a5hviozn9EpUUt40wLcgad6z5Y8Zu7Xc4TIVlsCdqNJ_eWACjJZXBzY0-FKgAXu55y6JoZNP6vwIuFpfLBPTGswWu_uGleXDwY3cJiyxvyIYuRgQpGW-i9rvqOAjqC-aO8n7UxclfFuo-390VSEwG8Qq3cHMsDop7J4VSr9hJKfd57KP_BWFui-vQlGc-VPDqOk10EBYiEDqjXPBzD2Gv2S5Xnwc0x1kqYhu0gT71k0A"
 
 const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(token);
@@ -10,9 +10,25 @@ function getMyData(){
     (async() => {
         const me = await spotifyApi.getMe();
         console.log(me.body);
+        getUserPlaylists(me.body.id)
     })().catch(e=>{
         console.error(e);
     });
 }
+
+// //GETS PLAYLISTS
+async function getUserPlaylists(userName){
+    const data = await spotifyApi.getUserPlaylists(userName, {
+        limit: 50
+    })
+
+    console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-")
+    let playlists = []
+
+    for (let playlist of data.body.items){
+        console.log(playlist.name + " " + playlist.id)
+    }
+}
+
 
 getMyData();
