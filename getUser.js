21ca49dbc603d3583b72334c6b9352e69f313660
mlfarn <mlfarn24@gmail.com>
2022-01-15 15:36:42 -0800

    Add printing of playlist tracks

--- getUser.js
+++ getUser.js
@@ -1,6 +1,6 @@
 const fs = require('fs')
 const SpotifyWebApi = require('spotify-web-api-node');
-const token = "BQCFA42UtWslfrB30FflT8ZMppKKgN-jaQ5uBZEwG_y0yrham9LH5n4lPGydkEVpoc8s_EQ_a5hviozn9EpUUt40wLcgad6z5Y8Zu7Xc4TIVlsCdqNJ_eWACjJZXBzY0-FKgAXu55y6JoZNP6vwIuFpfLBPTGswWu_uGleXDwY3cJiyxvyIYuRgQpGW-i9rvqOAjqC-aO8n7UxclfFuo-390VSEwG8Qq3cHMsDop7J4VSr9hJKfd57KP_BWFui-vQlGc-VPDqOk10EBYiEDqjXPBzD2Gv2S5Xnwc0x1kqYhu0gT71k0A"
+const token = "BQDbx1GR35MJT7F5mORyt5GGNsiI0Ku6bpms6NKfvuj-CBT-j7WurZBdb_Ft1YI1SsLzFXuMnpLN8i7_qQKbq0UPJFDuLzV1LTuZn2xOsiJYep1sLi4StZPZKm6Tb8QXnDWSTuQkOS9sNsKaBhQSz36UJu9zgrfBd2EF0JuZmtGqhKekXG751YdFjiU2NY3wrrrmN3I3dgN6id6Qu_6C2IXp0kVs509R76dc4pUWpqTfCY-nyer_R-jqIzZ7Bj7M1yupePH89QIQr9preA8KcsP3i7aHSYY"
 
 const spotifyApi = new SpotifyWebApi();
 spotifyApi.setAccessToken(token);
@@ -19,7 +19,7 @@ function getMyData(){
 // //GETS PLAYLISTS
 async function getUserPlaylists(userName){
     const data = await spotifyApi.getUserPlaylists(userName, {
-        limit: 50
+        limit: 2 // change to 50 later
     })
 
     console.log("-+-+-+-+-+-+-+-+-+-+-+-+-+-")
@@ -27,7 +27,31 @@ async function getUserPlaylists(userName){
 
     for (let playlist of data.body.items){
         console.log(playlist.name + " " + playlist.id)
+
+        let tracks = await getPlaylistTracks(playlist.id, playlist.name);
+        //console.log(tracks);
     }
+}
+
+async function getPlaylistTracks(playlistId, playlistName) {
+    const data = await spotifyApi.getPlaylistTracks(playlistId, {
+        offset: 0,
+        limit: 100,
+        fields: 'items'
+    })
+
+    //console.log('The playlist contains these tracks', data.body);
+    console.log("'" + playlistName + "'" + ' contains these tracks:');
+    let tracks = [];
+
+    for (let track_obj of data.body.items) {
+        const track = track_obj.track
+        tracks.push(track);
+        console.log(track.name + " : " + track.artists[0].name)
+    }
+
+    console.log("-------------++++");
+    return tracks;
 }
 
 
